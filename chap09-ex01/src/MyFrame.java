e37fe3894fdb83d74c668fa2ea94db960731952e
USER <USER@AMP-28>
2019-04-18 14:28:29 +0900

    Commit

---
+++ chap09-ex01/src/MyFrame.java
@@ -0,0 +1,15 @@
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
+	public MyFrame() {
+		setTitle("«¡∑π¿”");
+		setSize(300, 300);
+		setVisible(true);
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyFrame frame = new MyFrame();
+	}
+
+}
