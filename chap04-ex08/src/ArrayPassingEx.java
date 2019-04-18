c9ded1b821c0a800949c60733271792a2c0f3135
USER <USER@AMP-28>
2019-04-18 12:14:12 +0900

    Commit

---
+++ chap04-ex08/src/ArrayPassingEx.java
@@ -0,0 +1,28 @@
+
+public class ArrayPassingEx {
+		static void replaceSpace(char a[]) { //배열 a의 공백문자를 ','로 변경
+				for(int i = 0; i<a.length; i++)
+						if(a[i] == ' ') // 공백 문자를 ','로 변경
+								a[i] = ',';
+				
+		}
+		
+		
+		static void printcharArray(char a[]) { // 배열 a의 문자들을 화면에 출력
+				for(int i = 0; i <a.length; i++)
+						System.out.print(a[i]); // 배열 원소 문자 출력
+				System.out.println(); // 배열 원소 모두 출력 후 줄바꿈
+		}
+		
+		
+			public static void main(String[] args) {
+					char c[] = {'T','h','i','s',' ','i','s',' ','a',' ','p','e','n','c','i','l','.'};
+					printcharArray(c); // 원래 배열 원소 출력
+					replaceSpace(c); // 공백 문자 바꾸기
+					printcharArray(c); // 수정된 배열 원소 출력
+			
+			
+			}
+		
+
+	}
