5e3d42c354ebee3ca8d664bf8ce2194878b62774
USER <USER@AMP-28>
2019-04-18 12:27:37 +0900

    Commit

---
+++ chap04-ex10/src/AccessEx.java
@@ -0,0 +1,16 @@
+class Sample {
+		public int a;
+		private int b;
+		int c;
+}
+
+
+public class AccessEx {
+	public static void main(String[] args) {
+			Sample sample = new Sample();
+			sample.a = 10;
+			sample.b = 10;
+			sample.c = 10;
+	}
+
+}
