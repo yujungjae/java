2ff215934a2634f462a404b077995c02bac1463c
USER <USER@AMP-28>
2019-04-25 12:42:55 +0900

    Conit

---
+++ ssss/src/ssss/jjjjj.java
@@ -0,0 +1,29 @@
+package ssss;
+
+import java.awt.Container;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+public class jjjjj extends JFrame {
+		public jjjjj () {
+			setTitle("자바연습");
+			setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			Container c = getContentPane();
+			
+			JMenuBar mb = new JMenuBar();
+			JMenu FileMenu = new JMenu("연습작(P)");
+			
+			JButton j =new JButton("연습작");
+			
+			
+		}
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
