8b281d821feb84f2c488a8d258b44fd0d97dc534
USER <USER@AMP-28>
2019-04-25 17:59:28 +0900

    Commit

---
+++ chap12-ex08/src/raphicsDrawImageEx3.java
@@ -0,0 +1,31 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class raphicsDrawImageEx3 extends JFrame {
+	private MyPanel panel = new MyPanel();
+	
+	public raphicsDrawImageEx3() {
+		setTitle("이미지 일부분을 크기 조절하여 그리기");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setContentPane(panel);
+		
+		setSize(300, 300);
+		setVisible(true);
+	}
+
+	class MyPanel extends JPanel {
+		private ImageIcon icon = new ImageIcon("images/image0.jpg");
+		private Image img = icon.getImage();
+		
+		public void paintComponent(Graphics g) {
+			super.paintComponent(g);
+			
+			
+			g.drawImage(img, 20, 20, 250, 100, 100, 50, 200, 200, this);
+		}
+	}
+	
+	public static void main(String [] args) {
+		new raphicsDrawImageEx3();
+	}
+} 
